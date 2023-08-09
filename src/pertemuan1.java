ccf27f5031b1c2367193ffe4ad67e8d74cb835e1
Thompsn <c14230089@john.petra.ac.id>
2023-08-09 14:27:14 +0700

    init

---
+++ src/pertemuan1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pertemuan1 {
+    public static void main(String[] args) {
+      System.out.println("---Data mahasiwa---");
+        System.out.println("Nama : Thompson jeferson");
+        System.out.println("NRP : C14230089");
+        System.out.println("Mata Kuliah : Algoritma dan Pemrograman");
+        System.out.println("Kelas : B");
+        System.out.println("Tahun 2023/2024");
+        System.out.println("Semester : Gasal");
+        System.out.println("-------------------------");
+
+
+    }
+}
